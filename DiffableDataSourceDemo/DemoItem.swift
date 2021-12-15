56f28ffea015f4d13650c2ef942fc5989434c941
Jeans Ruiz <jeansruiz.c@gmail.com>
2021-12-15 09:05:14 -0500

    Add enum to control available viewsC

--- DiffableDataSourceDemo/DemoItem.swift
+++ DiffableDataSourceDemo/DemoItem.swift
@@ -6,26 +6,42 @@
 //
 
 import Foundation
+import UIKit
 
 struct DemoItem: Hashable {
   let name: String
-  let identifier: String
+  let viewType: ViewScenes
 
-  init(name: String, identifier: String) {
+  init(name: String, viewType: ViewScenes) {
     self.name = name
-    self.identifier = identifier
+    self.viewType = viewType
   }
 
 //  func hash(into hasher: inout Hasher) {
 //    hasher.combine(identifier)
 //  }
 
-  static func == (lhs: DemoItem, rhs: DemoItem) -> Bool {
-    return lhs.name == rhs.name &&
-      lhs.identifier == rhs.identifier
-  }
+  // ‼️ Because I uses native types
+  // This method is not needed 👇
+
+//  static func == (lhs: DemoItem, rhs: DemoItem) -> Bool {
+//    return lhs.name == rhs.name &&
+//      lhs.identifier == rhs.identifier
+//  }
 }
 
 enum Section {
   case main
+}
+
+// MARK: - Available scenes
+enum ViewScenes: String {
+  case diffTable
+
+  func getInstance() -> UIViewController {
+    switch self {
+    case .diffTable:
+      return DiffTableViewController()
+    }
+  }
 }
