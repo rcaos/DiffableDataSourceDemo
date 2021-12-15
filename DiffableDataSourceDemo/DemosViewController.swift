d627fdabda9819d077e83531d27851652b2b36a5
Jeans Ruiz <jeansruiz.c@gmail.com>
2021-12-15 06:58:08 -0500

    Configure Basic tableView

--- DiffableDataSourceDemo/DemosViewController.swift
+++ DiffableDataSourceDemo/DemosViewController.swift
@@ -7,36 +7,75 @@
 
 import UIKit
 
-struct DemoItem {
+struct DemoItem: Hashable {
   let name: String
-  let controllerClass: UIViewController.Type
-  let controllerIdentifier: String?
+  let identifier: String
 
-  init(name: String, controllerClass: UIViewController.Type, controllerIdentifier: String? = nil) {
+  init(name: String, identifier: String) {
     self.name = name
-    self.controllerClass = controllerClass
-    self.controllerIdentifier = controllerIdentifier
+    self.identifier = identifier
   }
+
+//  func hash(into hasher: inout Hasher) {
+//    hasher.combine(identifier)
+//  }
+
+  static func == (lhs: DemoItem, rhs: DemoItem) -> Bool {
+    return lhs.name == rhs.name &&
+      lhs.identifier == rhs.identifier
+  }
+}
+
+enum Section {
+  case main
 }
 
 class DemosViewController: UIViewController {
 
-  let collectionView = UICollectionView(frame: .zero)
+  let tableView = UITableView(frame: .zero)
 
   let demos: [DemoItem] = [
     DemoItem(name: "Diff Algorithm",
-             controllerClass: DiffTableViewController.self)
+             identifier: DiffTableViewController.self.description())
   ]
 
   override func viewDidLoad() {
     super.viewDidLoad()
     title = "Demos"
-    view.addSubview(collectionView)
+    view.addSubview(tableView)
+
+
+    tableView.register(UITableViewCell.self, forCellReuseIdentifier: reuseIdentifier)
+
+    setupDataSource()
+    populatedItems()
   }
 
   override func viewDidLayoutSubviews() {
     super.viewDidLayoutSubviews()
-    collectionView.frame = view.bounds
+    tableView.frame = view.bounds
+  }
+
+  typealias DataSource = UITableViewDiffableDataSource<Section, DemoItem>
+  typealias Snapshot = NSDiffableDataSourceSnapshot<Section, DemoItem>
+
+  private var dataSource: DataSource!
+
+  private let reuseIdentifier = "tableIdentifier"
+
+  func setupDataSource() {
+    dataSource = UITableViewDiffableDataSource(tableView: tableView, cellProvider: { (tv, index, item) -> UITableViewCell? in
+      let cell = tv.dequeueReusableCell(withIdentifier: self.reuseIdentifier, for: index)
+      cell.textLabel?.text = item.name
+      cell.accessoryType = .disclosureIndicator
+      return cell
+    })
+  }
+
+  func populatedItems() {
+    var snapshot = Snapshot()
+    snapshot.appendSections([.main])
+    snapshot.appendItems(demos)
+    dataSource.apply(snapshot)
   }
 }
-
