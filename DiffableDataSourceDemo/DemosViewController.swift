e603c4f2e6ed3e7c35576a6e0461a1fc4c50373b
Jeans Ruiz <jeansruiz.c@gmail.com>
2021-12-15 09:05:26 -0500

    Allow table selection

--- DiffableDataSourceDemo/DemosViewController.swift
+++ DiffableDataSourceDemo/DemosViewController.swift
@@ -8,52 +8,67 @@
 import UIKit
 
 class DemosViewController: UIViewController {
-
+  
   let tableView = UITableView(frame: .zero)
-
+  
   let demos: [DemoItem] = [
     DemoItem(name: "Diff Algorithm",
-             identifier: DiffTableViewController.self.description())
+             viewType: .diffTable)
   ]
-
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     title = "Demos"
     view.addSubview(tableView)
     setupTable()
   }
-
+  
   override func viewDidLayoutSubviews() {
     super.viewDidLayoutSubviews()
     tableView.frame = view.bounds
   }
-
+  
   private func setupTable() {
     tableView.register(UITableViewCell.self, forCellReuseIdentifier: reuseIdentifier) // 1
+    tableView.delegate = self
+    
     setupDataSource() // 2
     populatedItems() // 3
   }
-
+  
   typealias DataSource = UITableViewDiffableDataSource<Section, DemoItem>
   typealias Snapshot = NSDiffableDataSourceSnapshot<Section, DemoItem>
-
+  
   private var dataSource: DataSource?
-
+  
   private let reuseIdentifier = "reuseTableIdentifier"
-
+  
   private func setupDataSource() {
     dataSource = UITableViewDiffableDataSource(tableView: tableView, cellProvider: { (tv, index, item) -> UITableViewCell? in
       let cell = tv.dequeueReusableCell(withIdentifier: self.reuseIdentifier, for: index)
       cell.textLabel?.text = item.name
       cell.accessoryType = .disclosureIndicator
+      cell.selectionStyle = .none
       return cell
     })
   }
-
+  
   private func populatedItems() {
     var snapshot = Snapshot()
     snapshot.appendSections([.main])
     snapshot.appendItems(demos)
     dataSource?.apply(snapshot)
+  }
+}
+
+// MARK: - UITableViewDelegate
+extension DemosViewController: UITableViewDelegate {
+  
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    let snapshot = dataSource?.snapshot()
+    let section = snapshot?.itemIdentifiers(inSection: .main) ?? []
+    let item = section[indexPath.row]
+    print("Has selected: [\(item)]")
+    navigationController?.pushViewController(item.viewType.getInstance(), animated: true)
   }
 }
