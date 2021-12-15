f2f050b43dc343389d0cc8b8330ddbb97e820c89
Jeans Ruiz <jeansruiz.c@gmail.com>
2021-12-15 15:10:05 -0500

    Add util Cell

---
+++ DiffableDataSourceDemo/Utils/LabelCell.swift
@@ -0,0 +1,81 @@
+//
+//  LabelCell.swift
+//  DiffableDataSourceDemo
+//
+//  Created by Jeans Ruiz on 15/12/21.
+//
+
+import UIKit
+
+final class LabelCell: UICollectionViewCell {
+  
+  fileprivate static let insets = UIEdgeInsets(top: 8, left: 15, bottom: 8, right: 15)
+  fileprivate static let font = UIFont.systemFont(ofSize: 17)
+  
+  static var singleLineHeight: CGFloat {
+    return font.lineHeight + insets.top + insets.bottom
+  }
+  
+  static func textHeight(_ text: String, width: CGFloat) -> CGFloat {
+    let constrainedSize = CGSize(width: width - insets.left - insets.right, height: CGFloat.greatestFiniteMagnitude)
+    let attributes = [ NSAttributedString.Key.font: font ]
+    let options: NSStringDrawingOptions = [.usesFontLeading, .usesLineFragmentOrigin]
+    let bounds = (text as NSString).boundingRect(with: constrainedSize, options: options, attributes: attributes, context: nil)
+    return ceil(bounds.height) + insets.top + insets.bottom
+  }
+  
+  fileprivate let label: UILabel = {
+    let label = UILabel()
+    label.backgroundColor = .clear
+    label.numberOfLines = 0
+    label.font = LabelCell.font
+    return label
+  }()
+  
+  let separator: CALayer = {
+    let layer = CALayer()
+    layer.backgroundColor = UIColor.separator.cgColor
+    return layer
+  }()
+  
+  var text: String? {
+    get {
+      return label.text
+    }
+    set {
+      label.text = newValue
+    }
+  }
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    contentView.backgroundColor = UIColor.systemBackground
+    contentView.addSubview(label)
+    contentView.layer.addSublayer(separator)
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  override func layoutSubviews() {
+    super.layoutSubviews()
+    let bounds = contentView.bounds
+    label.frame = bounds.inset(by: LabelCell.insets)
+    let height: CGFloat = 0.5
+    let left = LabelCell.insets.left
+    separator.frame = CGRect(x: left, y: bounds.height - height, width: bounds.width - left, height: height)
+  }
+  
+  override var isHighlighted: Bool {
+    didSet {
+      let color = isHighlighted ? UIColor.gray.withAlphaComponent(0.3) : UIColor.clear
+      contentView.backgroundColor = color
+    }
+  }
+  
+  //  override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+  //    super.traitCollectionDidChange(previousTraitCollection)
+  //    separator.backgroundColor = UIColor.separator.cgColor
+  //  }
+}
